b8a0c1dfdbcab166ff78495f0a476baa8a0ec06f
vpvm96 <vpvm96@gmail.com>
2023-04-19 19:17:14 +0900

    feat: study font in next13 & apply font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import { Metadata } from "next"
 import Link from "next/link"
 import "./globals.css"
+import { Open_Sans, Nanum_Gothic } from "next/font/google"
+
+const openSans = Open_Sans({ subsets: ["latin"] })
+const nanumGothic = Nanum_Gothic({
+  weight: "700",
+  subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "Next13 - Blog",
@@ -16,10 +23,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" style={openSans.style}>
       <body>
         <header className="flex items-center justify-between p-6 border-b-2 border-blue-700">
-          <h1 className="text-4xl">Practice Note</h1>
+          <h1 className="text-4xl" style={nanumGothic.style}>
+            Practice Note
+          </h1>
           <nav className="flex justify-between gap-3">
             <Link href="/products">Products</Link>
             <Link href="/about">About</Link>
